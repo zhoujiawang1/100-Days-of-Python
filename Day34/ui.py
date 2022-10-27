150741a8e80fb896ede7aa077167f0d73fcecf04
Chow <Tony.Chow@ds.nrc.ca>
2022-10-27 13:39:50 -0400

    Day 36 modif

--- Day34/ui.py
+++ Day34/ui.py
@@ -26,11 +26,13 @@ class QuizInterface:
         )
 
         true_image = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button = Button(
+            image=true_image, highlightthickness=0, command=self.true_pressed)
         self.true_button.grid(row=2, column=0)
 
         false_image = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button = Button(
+            image=false_image, highlightthickness=0, command=self.false_pressed)
         self.false_button.grid(row=2, column=1)
 
         self.canvas.grid(row=1, column=0, columnspan=2)
@@ -42,3 +44,12 @@ class QuizInterface:
     def get_next_question(self):
         q_text = self.quiz.next_question()
         self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def true_pressed(self):
+        self.quiz.check_answer("True")
+
+    def false_pressed(self):
+        self.quiz.check_answer("False")
+
+    def update_score(self):
+        self.score_label.config(text=f"Score: {self.quiz.score}")
--- Day36/main.py
+++ Day36/main.py
@@ -1,48 +1,78 @@
 import requests
 import pandas
 from datetime import datetime, timedelta
+from newsapi import NewsApiClient
+import html
 
-STOCK = "TSLA"
+STOCK = "BITO"
 COMPANY_NAME = "Tesla Inc"
 API_KEY1 = "EW4VQ380ZJHR9WJD"
+API_KEY2 = "54da6dce5a454b889681940d67349ff0"
 
 # STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
 # getting yesterday's price
-url1 = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={STOCK}&interval=5min&apikey={API_KEY1}"
+url1 = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={STOCK}&interval=30min&apikey={API_KEY1}"
 r1 = requests.get(url1)
 r1.raise_for_status()
 stock_data1 = r1.json()
 today = datetime.today()
-yesterday = (today - timedelta(days=1))
-yesterday = str(yesterday).split(" ")[0]
+day_open = (today - timedelta(days=1))
 
-yest_price = float(stock_data1["Time Series (5min)"]
-                   [f"{yesterday} 16:00:00"]["4. close"])
+day_open = str(day_open).split(" ")[0]
 
+today_price = float(stock_data1["Time Series (30min)"]
+                    [f"{day_open} 09:30:00"]["1. open"])
+print(f"Current Bitcoin {STOCK} Price Open: {today_price}")
 
 # getting today's price
 url2 = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol={STOCK}&interval=15min&slice=year1month1&apikey={API_KEY1}"
+
+prev_day_close = (today - timedelta(days=2))
+prev_day_close = str(prev_day_close).split(" ")[0]
 with requests.Session() as s:
     download = s.get(url2).content
     stock_data2 = pandas.read_csv(url2)
-    print(stock_data2)
+    yest_price = float(stock_data2.loc[stock_data2['time']
+                                       == f"{prev_day_close} 16:00:00"]["close"].values[0])
+    print(f"Yesterday's Close {STOCK} Price: {yest_price}")
 
-
-# today = str(today).split(" ")[0]
-
-# yest_price = float(stock_data2["Time Series (15min)"]
-#                    [f"{today} 9:00:00"]["1. open"])
-
+if abs((today_price-yest_price)/yest_price) >= 0.05:
+    print("Get NEWS")
 
 # STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
 
+# Init
+newsapi = NewsApiClient(api_key=API_KEY2)
+
+# /v2/top-headlines
+top_headlines = newsapi.get_top_headlines(q="Tesla",
+                                          language='en'
+                                          )
+all_articles = newsapi.get_everything(q="bitcoin",
+                                      from_param=day_open,
+                                      to=day_open,
+                                      language='en',
+                                      sort_by='popularity',
+                                      page=1)
+
+
+titles = []
+descriptions = []
+# print(top_headlines)
+for num_article in range(2):
+    title = all_articles["articles"][num_article]["title"]
+    desc = all_articles["articles"][num_article]["description"]
+    # titles.append(title)
+    # descriptions.append(desc)
+    print(f"Headline: {title}\nDescription: {html.unescape(desc)}")
+
+
 # STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number.
-
 
 # Optional: Format the SMS message like this:
 """
